7593e153da3dd2b0666f4ee8337d26e8e905128f
Brian Lauber <brian.lauber@33across.com>
2018-11-04 22:56:11 -0500

    Just playing around w/ the Card element

--- client/src/comps/Wishlist/index.js
+++ client/src/comps/Wishlist/index.js
@@ -1,8 +1,63 @@
 
 import React from "react";
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
+import TextField from '@material-ui/core/TextField';
+
+import styled from 'styled-components';
+
+const StyledCard = styled(Card)`
+  min-width: 250px;
+  max-width: 800px;
+  margin: auto;
+`
+
 const Wishlist = ()=> (
-  <div>Wishes!</div>
+  <div>
+    <StyledCard >
+      <CardContent>
+        <div>
+          <TextField
+            label="Name"
+            variant="outlined"
+          />
+        </div>
+
+        <div>
+          <TextField
+            label="Description"
+            multiline
+            variant="outlined"
+          />
+        </div>
+      </CardContent>
+      
+    </StyledCard>
+
+    <StyledCard >
+      <CardContent>
+        <div>
+          <TextField
+            label="Name"
+            variant="outlined"
+          />
+        </div>
+
+        <div>
+          <TextField
+            label="Description"
+            multiline
+            variant="outlined"
+          />
+        </div>
+      </CardContent>
+      
+    </StyledCard>
+  </div>
 );
 
 export default Wishlist;
