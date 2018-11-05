be6bb825b820d2825f47847175f0fa83add9c5bf
Brian Lauber <brian.lauber@33across.com>
2018-11-04 23:37:29 -0500

    Friend list

--- client/src/comps/Friends/index.js
+++ client/src/comps/Friends/index.js
@@ -1,8 +1,31 @@
 
 import React from "react";
 
+import List from '@material-ui/core/List';
+
+// import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const friends = [
+  {name: "Cindy Lauber"},
+  {name: "Mark Lauber"},
+  {name: "Matt Lauber"},
+  {name: "Mike Lauber"},
+  {name: "William Lauber"},
+];
+
+
 const Friends = ()=> (
-  <div>Friends!</div>
+  <List>
+    {friends.map((f, i)=> (
+      <ListItem key={i} button >
+        <ListItemText primary={f.name} />
+      </ListItem>
+    ))}
+  </List>
 );
 
 export default Friends;
